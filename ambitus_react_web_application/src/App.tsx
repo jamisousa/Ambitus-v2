0dc6d78544ffc1248c137093207480ff8f0f3fbf
jamisousa <jamiledsousa@gmail.com>
2023-10-14 14:09:21 -0300

    feat: adding home page redirect to login

--- ambitus_react_web_application/src/App.tsx
+++ ambitus_react_web_application/src/App.tsx
@@ -1,8 +1,18 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 function App() {
 
+  //TODO: Check if user token is still valid so they can enter dashboard
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    navigate("/login");
+  })
+  
   return (
     <>
-      <p>Ambitus</p>
+      <p>Loading...</p>
     </>
   )
 }
