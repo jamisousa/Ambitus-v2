41bda8239c33ac8b76f12ef3d00124b97b84f70c
jamisousa <jamiledsousa@gmail.com>
2023-12-17 13:56:03 -0300

    chore: changing main page

--- ambitus_mobile_application/lib/main.dart
+++ ambitus_mobile_application/lib/main.dart
@@ -26,6 +26,6 @@ class MyApp extends StatelessWidget {
             brightness: Brightness.light,
             useMaterial3: true,
             colorSchemeSeed: Colors.green),
-        home: ProfilePage());
+        home: IndexPage());
   }
 }
